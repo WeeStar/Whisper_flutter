6d26bbe4f2ee6f8849a32966652645da1f101ef8
WeeStar <402284187>
2021-02-02 11:06:32 +0800

    歌单详情页面 fab展示隐藏

--- lib/view/sheet_view/info/sheet_info_view.dart
+++ lib/view/sheet_view/info/sheet_info_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter_custom_dialog/flutter_custom_dialog.dart';
 import 'package:whisper/model/sheet_model.dart';
 import 'package:whisper/service/data/my_sheets_data_service.dart';
@@ -25,7 +26,11 @@ class _SheetInfoViewState extends State<SheetInfoView>
   String _coverImg; //封面图片用于避开setState重新加载
   String _title; //歌单标题用于避开setState重新加载
 
-  String title = "歌单"; //用于标题展示
+  //滚动检测
+  double _width = 100.0;
+  String _viewTitle = "歌单"; //用于标题展示
+  ScrollController _controller = ScrollController();
+  AnimationController _hideFabAnimation;
 
   bool _isLoadingData = true; //加载数据节流
   bool _isSyncData = false; //同步歌单节流
@@ -42,6 +47,11 @@ class _SheetInfoViewState extends State<SheetInfoView>
   @override
   void initState() {
     super.initState();
+
+    //滚动与fab显示隐藏动画
+    _controller.addListener(_listener);
+    _hideFabAnimation =
+        AnimationController(vsync: this, duration: kThemeAnimationDuration);
 
     if (!_sheetInfo.is_my && (_sheetInfo.tracks?.length ?? 0) == 0) {
       //网络歌单 请求信息
@@ -136,6 +146,7 @@ class _SheetInfoViewState extends State<SheetInfoView>
     }
   }
 
+  //删除歌单
   void _delSheet() {
     if (!_sheetInfo.is_my) return;
 
@@ -154,33 +165,122 @@ class _SheetInfoViewState extends State<SheetInfoView>
     });
   }
 
+  ListView _buildList() {
+    var list = ListView.builder(
+      controller: _controller,
+      itemCount: _isLoadingData ? 3 : ((_sheetInfo.tracks?.length ?? 0) + 2),
+      itemBuilder: (context, idx) {
+        if (idx == 0) {
+          return SheetInfoCoverView(
+              _coverImg, _title, _sheetInfo.tracks?.length ?? 0);
+        } else if (idx == 1) {
+          return Divider(
+            height: 1,
+          );
+        } else {
+          if (_isLoadingData) {
+            //等待图
+            return CommonView.buildLoadingView(context);
+          } else {
+            return InkWell(
+                child: MusicItemView(_sheetInfo.tracks[idx - 2],
+                    musicIdx: idx - 1),
+                onTap: () {
+                  if (_sheetInfo.tracks == null ||
+                      _sheetInfo.tracks.length == 0) return;
+
+                  //无效歌曲跳出
+                  if (!_sheetInfo.tracks[idx - 2].isPlayable()) return;
+
+                  //播放选中歌曲
+                  PlayerService.play(
+                      music: _sheetInfo.tracks[idx - 2], sheet: _sheetInfo);
+                  Navigator.push(context, MaterialPageRoute(builder: (context) {
+                    return PlayerView();
+                  }));
+                });
+          }
+        }
+      },
+    );
+    return list;
+  }
+
+  //滚动监听
+  void _listener() {
+    //向下滚动隐藏fab
+    switch (_controller.position.userScrollDirection) {
+      case ScrollDirection.forward:
+        if (_controller.position.maxScrollExtent !=
+            _controller.position.minScrollExtent) {
+          _hideFabAnimation.reverse();
+        }
+        break;
+      case ScrollDirection.reverse:
+        if (_controller.position.maxScrollExtent !=
+            _controller.position.minScrollExtent) {
+          _hideFabAnimation.forward();
+        }
+        break;
+      case ScrollDirection.idle:
+        break;
+    }
+
+    //滚动修改标题
+    if (_title.isEmpty) return;
+    var offset = _controller.offset;
+
+    //变化标题
+    if (offset > _width * 0.25 && _viewTitle == "歌单") {
+      setState(() {
+        _viewTitle = _title;
+      });
+    } else if (offset < _width * 0.25 && _viewTitle != "歌单") {
+      setState(() {
+        _viewTitle = "歌单";
+      });
+    }
+  }
+
+  //构造fab
+  Widget _buildFab() {
+    return Container(
+        child: ScaleTransition(
+            scale: Tween(begin: 1.0, end: 0.0).animate(_hideFabAnimation)
+              ..addListener(() {
+                setState(() {});
+              }),
+            alignment: Alignment.center,
+            child: FloatingActionButton(
+              tooltip: "播放全部",
+              child: Icon(
+                Icons.play_arrow,
+                size: 35,
+              ),
+              onPressed: () {
+                //播放全部歌曲
+                PlayerService.play(sheet: _sheetInfo);
+                Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  return PlayerView();
+                }));
+              },
+            )));
+  }
+
   @override
   Widget build(BuildContext context) {
     YYDialog.init(context);
-    var width = MediaQuery.of(context).size.width;
+    _width = MediaQuery.of(context).size.width;
 
     return Scaffold(
-        floatingActionButton: FloatingActionButton(
-          tooltip: "播放全部",
-          child: Icon(
-            Icons.play_arrow,
-            size: 35,
-          ),
-          onPressed: () {
-            //播放全部歌曲
-            PlayerService.play(sheet: _sheetInfo);
-            Navigator.push(context, MaterialPageRoute(builder: (context) {
-              return PlayerView();
-            }));
-          },
-        ),
+        floatingActionButton: _buildFab(),
         floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
         appBar: PreferredSize(
             preferredSize: Size.fromHeight(50),
             child: AppBar(
                 title: Text(
-                  title,
-                  style: TextStyle(fontSize: title == "歌单" ? 20 : 15),
+                  _viewTitle,
+                  style: TextStyle(fontSize: _viewTitle == "歌单" ? 20 : 15),
                 ),
                 actions: <Widget>[
                   _sheetInfo.is_my
@@ -203,63 +303,13 @@ class _SheetInfoViewState extends State<SheetInfoView>
                           tooltip: "分享歌单",
                           onPressed: () {}),
                 ])),
-        body: new NotificationListener(
-          onNotification: (ScrollNotification note) {
-            //监听滚动位置
-            if (_title.isEmpty) return true;
-            var offset = note.metrics.pixels.toInt();
+        body: _buildList());
+  }
 
-            //变化标题
-            if (offset > width * 0.25 && title == "歌单") {
-              setState(() {
-                title = _title;
-              });
-            } else if (offset < width * 0.25 && title != "歌单") {
-              setState(() {
-                title = "歌单";
-              });
-            }
-            return true;
-          },
-          child: ListView.builder(
-            itemCount:
-                _isLoadingData ? 3 : ((_sheetInfo.tracks?.length ?? 0) + 2),
-            itemBuilder: (context, idx) {
-              if (idx == 0) {
-                return SheetInfoCoverView(
-                    _coverImg, _title, _sheetInfo.tracks?.length ?? 0);
-              } else if (idx == 1) {
-                return Divider(
-                  height: 1,
-                );
-              } else {
-                if (_isLoadingData) {
-                  //等待图
-                  return CommonView.buildLoadingView(context);
-                } else {
-                  return InkWell(
-                      child: MusicItemView(_sheetInfo.tracks[idx - 2],
-                          musicIdx: idx - 1),
-                      onTap: () {
-                        if (_sheetInfo.tracks == null ||
-                            _sheetInfo.tracks.length == 0) return;
-
-                        //无效歌曲跳出
-                        if (!_sheetInfo.tracks[idx - 2].isPlayable()) return;
-
-                        //播放选中歌曲
-                        PlayerService.play(
-                            music: _sheetInfo.tracks[idx - 2],
-                            sheet: _sheetInfo);
-                        Navigator.push(context,
-                            MaterialPageRoute(builder: (context) {
-                          return PlayerView();
-                        }));
-                      });
-                }
-              }
-            },
-          ),
-        ));
+  @override
+  void dispose() {
+    _controller.dispose();
+    _hideFabAnimation.dispose();
+    super.dispose();
   }
 }
